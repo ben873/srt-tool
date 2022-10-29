67a63e2e5fa54a6e95b278c43576c875ef55ccb5
Ben Dotson <djnebulla@gmail.com>
2022-10-29 14:02:10 -0400

    Add fps calc and xlsx export/download

--- src/index.js
+++ src/index.js
@@ -1,24 +1,79 @@
 import srtParser2 from "srt-parser-2";
+import ExcelExport from 'export-xlsx';
+
+import { SETTINGS_FOR_EXPORT } from "./xlsSettings";
 
 const textBox = document.getElementById('input');
 const submit = document.getElementById('btn1');
+const fpsSelect = document.getElementById('fpsselect');
 var srt = "";
+var parsedData;
 
 function processSrt(rawData){
     
     var parser = new srtParser2();
-    var result = parser.fromSrt(rawData);
-    console.log(generateCsv(result));
-    document.getElementById('responce').innerHTML = generateCsv(result);
+    parsedData = parser.fromSrt(rawData);
+    console.log(generateCsv(parsedData));
+    document.getElementById('responce').innerHTML = generateCsv(parsedData);
+}
+
+function calculateFrame(milliseconds) {
+    const fps = fpsSelect.value;
+    const seconds = milliseconds / 1000.0;
+
+    return fps * seconds;
+}
+
+function convertTimecode(timeCode) {
+    const elements = timeCode.split(',');
+    const [baseTime, milliseconds] = elements;
+
+    const frame = Math.round(calculateFrame(milliseconds));
+
+    return `${baseTime}:${frame}`;
 }
 
 function generateCsv(data) {
-    return data.map((line)=>(`${line.startTime},${line.startSeconds}`)).join("\n");
+    const csvLines = data.map((line)=>{
+        const { id, startTime, endTime, text } = line;
+        const convertedTcIn = convertTimecode(startTime);
+        const convertedTcOut = convertTimecode(endTime);
+
+        return `${id},${convertedTcIn},${convertedTcOut},${text}`;
+    })
+
+    return csvLines.join("\n");
+}
+
+function generateXLSData(data) {
+    return data.map((line)=>{
+        const { id, startTime, endTime, text } = line;
+        const tcIn = convertTimecode(startTime);
+        const tcOut = convertTimecode(endTime);
+
+        return { id, tcIn, tcOut };
+    })
+}
+
+function downloadXLS() {
+    if(!parsedData) return;
+
+    const xlsData = generateXLSData(parsedData);
+    const data = [
+        {
+            table1: xlsData
+        }
+    ];
+
+    const excelExport = new ExcelExport();
+    excelExport.downloadExcel(SETTINGS_FOR_EXPORT, data);
+
 }
 
 function getSrt(){
     srt = textBox.value;
     processSrt(srt);
+    downloadXLS();
 }
 
 
---
+++ src/xlsSettings.js
@@ -0,0 +1,36 @@
+import { alignment, defaultDataType } from 'export-xlsx';
+
+// Export settings
+export const SETTINGS_FOR_EXPORT = {
+  // Table settings
+  fileName: 'example',
+  workSheets: [
+    {
+      sheetName: 'example',
+      startingRowNumber: 1,
+      gapBetweenTwoTables: 0,
+      tableSettings: {
+        table1: {
+          headerDefinition: [
+            {
+              name: 'Sub Id',
+              key: 'id',
+            },
+            {
+                name: 'Timecode In',
+                key: 'tcIn',
+            },
+            {
+                name: 'Timecode OUT',
+                key: 'tcOut',
+            },
+            {
+                name: 'Line',
+                key: 'Text',
+            },
+          ],
+        }
+      }
+    },
+  ],
+};
